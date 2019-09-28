90b70f576bcbd44f6cd639e714b964b2e539d79d
not-saved <corradin.loris@gmail.com>
2019-09-28 18:29:36 +0200

    Changes

--- client/src/components/gameList/GameList.js
+++ client/src/components/gameList/GameList.js
@@ -9,7 +9,7 @@ const GameList = ({ games, openModal }) => {
 		<div className="top50">
 			<div
 				className="ui container centered text scrolling"
-				style={{ maxHeight: height - 50 }}
+				style={{ maxHeight: height - 92 }}
 			>
 				{games.map((game, idx) => (
 					<GameCard
