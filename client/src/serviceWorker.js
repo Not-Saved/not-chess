ac0fa43ec3ed5b63208bc1c614e0d2736db47c19
not-saved <corradin.loris@gmail.com>
2019-11-06 11:50:00 +0100

    Second attempt

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -34,13 +34,14 @@ export function register(config) {
 			// serve assets; see https://github.com/facebook/create-react-app/issues/2374
 			return;
 		}
-		if (isAuthRoute()) {
-			return;
-		}
 
 		window.addEventListener("load", () => {
 			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
+			if (isAuthRoute()) {
+				console.log("unregistering service worker for admin route");
+				unregister();
+				return false;
+			}
 			if (isLocalhost) {
 				// This is running on localhost. Let's check if a service worker still exists or not.
 				checkValidServiceWorker(swUrl, config);
